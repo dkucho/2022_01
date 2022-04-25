1f76464b89012f7467a40aa1722209a60829747d
dkucho <101546565+dkucho@users.noreply.github.com>
2022-04-25 17:03:46 +0900

    Add files via upload

---
+++ CH07/CH07-HW2.py
@@ -0,0 +1,27 @@
+money=0
+
+print("1.자장면:5500원")
+print("2.짬뽕:6000원")
+print("3.볶음밥::7000원")
+print("4.자장면:15000원")
+print("5.주문종료")
+
+
+
+while True:
+    name=input("메뉴 번호를 선택해주세요: ")
+    if name=='1':
+        money+=5500
+    elif name=='2':
+        money+=6000
+    elif name=='3':
+        money+=7000
+    elif name=='4':
+        money+=15000
+    else:
+        name=='5'
+        print("주문종료")
+        break
+
+print("")
+print("총 주문금액 : " + str(money) + "원")
